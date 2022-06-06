832753da032dc1bc4fd67bd67fcb6ed8795ece1f
hellosknath <s.nath32@yahoo.com>
2022-06-06 16:51:06 +0600

    it is serial thread

--- UdemyAndroidLearning/app/src/main/java/com/sriidea/udemyandroidlearning/MainActivity.kt
+++ UdemyAndroidLearning/app/src/main/java/com/sriidea/udemyandroidlearning/MainActivity.kt
@@ -1,59 +1,39 @@
 package com.sriidea.udemyandroidlearning
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
-    private var count = 0
-    private lateinit var userMessage: TextView
-    private lateinit var countMsg: TextView
-    private lateinit var btnDownload: Button
-    private lateinit var btnCount: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userMessage = findViewById(R.id.tvUserMessage)
-        countMsg = findViewById(R.id.tvCount)
-        btnCount = findViewById(R.id.btnCount)
-        btnDownload = findViewById(R.id.btnDownloadUserData)
-
-        btnCount.setOnClickListener {
-            countMsg.text = count++.toString()
-        }
-
-        btnDownload.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                btnDownloadUserData()
-            }
-
+        CoroutineScope(IO).launch {
+            Log.i(TAG, "onCreate: Calculation Started...")
+            val stock1 = getStock1()
+            val stock2 = getStock2()
+            val totalValue = stock1 + stock2
+            Log.i(TAG, "onCreate: Total is $totalValue")
         }
 
     }
 
-    private suspend fun btnDownloadUserData() {
-        for (i in 1..2000) {
-            for (j in 1..10) {
-                withContext(Dispatchers.Main) {
-                    var dt = ""
-                    dt += "$i x $j = ${i * j}\n"
-                    userMessage.text = dt
-                    Log.i(TAG, dt)
-                }
-            }
-        }
+    private suspend fun getStock1(): Int {
+        delay(10000)
+        Log.i(TAG, "getStock1: return")
+        return 10000
+    }
+
+    private suspend fun getStock2(): Int {
+        delay(8000)
+        Log.i(TAG, "getStock2: return")
+        return 8000
     }
 }
