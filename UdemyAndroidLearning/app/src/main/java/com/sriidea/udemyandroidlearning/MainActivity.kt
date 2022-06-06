e20267b5279a68bc53152e236fca02b497505969
hellosknath <s.nath32@yahoo.com>
2022-06-06 17:22:48 +0600

    unstructured concurrency

--- UdemyAndroidLearning/app/src/main/java/com/sriidea/udemyandroidlearning/MainActivity.kt
+++ UdemyAndroidLearning/app/src/main/java/com/sriidea/udemyandroidlearning/MainActivity.kt
@@ -2,45 +2,54 @@ package com.sriidea.udemyandroidlearning
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
+    private var count = 0
+    private lateinit var userMessage: TextView
+    private lateinit var countMsg: TextView
+    private lateinit var btnDownload: Button
+    private lateinit var btnCount: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Main).launch {
-            Log.i(TAG, "onCreate: Calculation Started...")
-            val stock1 = async(IO) { getStock1() }
-            val stock2 = async(IO) { getStock2() }
-            val totalValue = stock1.await() + stock2.await()
-            Toast.makeText(
-                this@MainActivity,
-                "onCreate: Total is $totalValue",
-                Toast.LENGTH_SHORT
-            ).show()
+        userMessage = findViewById(R.id.tvUserMessage)
+        countMsg = findViewById(R.id.tvCount)
+        btnCount = findViewById(R.id.btnCount)
+        btnDownload = findViewById(R.id.btnDownloadUserData)
+
+        btnCount.setOnClickListener {
+            countMsg.text = count++.toString()
+        }
+
+        btnDownload.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                userMessage.text = UserDataManager().getTotalUserCount().toString()
+            }
+
         }
 
     }
 
-    private suspend fun getStock1(): Int {
-        delay(10000)
-        Log.i(TAG, "getStock1: return")
-        return 10000
-    }
-
-    private suspend fun getStock2(): Int {
-        delay(8000)
-        Log.i(TAG, "getStock2: return")
-        return 8000
+    private suspend fun btnDownloadUserData() {
+        for (i in 1..2000) {
+            for (j in 1..10) {
+                withContext(Main) {
+                    var dt = ""
+                    dt += "$i x $j = ${i * j}\n"
+                    userMessage.text = dt
+                    Log.i(TAG, dt)
+                }
+            }
+        }
     }
 }
