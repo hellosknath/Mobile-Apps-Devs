38535cab51d0b280672e507cacfd0b33acd9c61b
hellosknath <s.nath32@yahoo.com>
2022-06-12 17:25:08 +0600

    adding constraint(network mode, charging mode)

--- UdemyAndroidLearning/app/src/main/java/com/sriidea/udemyandroidlearning/MainActivity.kt
+++ UdemyAndroidLearning/app/src/main/java/com/sriidea/udemyandroidlearning/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Observer
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 
@@ -24,9 +26,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun onetimeWorkRequest() {
         val workManager: WorkManager = WorkManager.getInstance(applicationContext)
-        val uploadRequest: OneTimeWorkRequest = OneTimeWorkRequest.Builder(
-            UploadWorker::class.java
-        ).build()
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val uploadRequest: OneTimeWorkRequest = OneTimeWorkRequest
+            .Builder(UploadWorker::class.java)
+            .setConstraints(constraints)
+            .build()
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
             .observe(this, Observer {
