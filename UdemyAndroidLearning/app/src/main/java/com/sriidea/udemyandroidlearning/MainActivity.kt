6ea6b473f9366d29c29db1a12f9e737ca55f6b27
hellosknath <s.nath32@yahoo.com>
2022-06-23 15:06:31 +0600

    android view binding

---
+++ UdemyAndroidLearning/app/src/main/java/com/sriidea/udemyandroidlearning/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.sriidea.udemyandroidlearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sriidea.udemyandroidlearning.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnStart.setOnClickListener {
+
+        }
+
+        binding.btnEnd.setOnClickListener {
+
+        }
+    }
+}
