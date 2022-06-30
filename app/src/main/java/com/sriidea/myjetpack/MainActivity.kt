2658711a24e72d90727a895a87a8e178c4508c75
hellosknath <s.nath32@yahoo.com>
2022-06-30 17:45:53 +0600

    using TextView Attributes

--- app/src/main/java/com/sriidea/myjetpack/MainActivity.kt
+++ app/src/main/java/com/sriidea/myjetpack/MainActivity.kt
@@ -3,13 +3,23 @@ package com.sriidea.myjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sriidea.myjetpack.ui.theme.MyJetPackTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,13 +41,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        fontSize = 20.sp,
+        color = Color.Red,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(color = Color.Green)
+            .border(2.dp, color = Color.Red)
+            .padding(10.dp)
+            .fillMaxWidth(0.5f)
+            .fillMaxHeight(0.3f)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyJetPackTheme {
-        Greeting("Srinath")
+        Greeting("Android")
     }
 }
