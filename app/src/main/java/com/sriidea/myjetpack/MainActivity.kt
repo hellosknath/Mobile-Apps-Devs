a20dca77513618ba9249cd500607ac9370cfda98
hellosknath <s.nath32@yahoo.com>
2022-07-14 13:11:30 +0600

    box layout example

--- app/src/main/java/com/sriidea/myjetpack/MainActivity.kt
+++ app/src/main/java/com/sriidea/myjetpack/MainActivity.kt
@@ -3,16 +3,17 @@ package com.sriidea.myjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,7 +25,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BoxExample2()
+            BoxExample3()
         }
     }
 }
@@ -54,60 +55,98 @@ fun BoxExample2(){
         .fillMaxSize()) {
 
         Text(style = MaterialTheme.typography.h6,
-        modifier = Modifier.background(Color.Yellow)
+        modifier = Modifier
+            .background(Color.Yellow)
             .padding(6.dp)
             .align(Alignment.TopCenter),
         text = "TopCenter")
 
         Text(style = MaterialTheme.typography.h6,
-            modifier = Modifier.background(Color.Yellow)
+            modifier = Modifier
+                .background(Color.Yellow)
                 .padding(6.dp)
                 .align(Alignment.TopStart),
             text = "TopStart")
 
 
         Text(style = MaterialTheme.typography.h6,
-            modifier = Modifier.background(Color.Yellow)
+            modifier = Modifier
+                .background(Color.Yellow)
                 .padding(6.dp)
                 .align(Alignment.TopEnd),
             text = "TopEnd")
 
         Text(style = MaterialTheme.typography.h6,
-            modifier = Modifier.background(Color.Yellow)
+            modifier = Modifier
+                .background(Color.Yellow)
                 .padding(6.dp)
                 .align(Alignment.CenterStart),
             text = "CenterStart")
 
         Text(style = MaterialTheme.typography.h6,
-            modifier = Modifier.background(Color.Yellow)
+            modifier = Modifier
+                .background(Color.Yellow)
                 .padding(6.dp)
                 .align(Alignment.Center),
             text = "Center")
 
         Text(style = MaterialTheme.typography.h6,
-            modifier = Modifier.background(Color.Yellow)
+            modifier = Modifier
+                .background(Color.Yellow)
                 .padding(6.dp)
                 .align(Alignment.CenterEnd),
             text = "CenterEnd")
 
         Text(style = MaterialTheme.typography.h6,
-            modifier = Modifier.background(Color.Yellow)
+            modifier = Modifier
+                .background(Color.Yellow)
                 .padding(6.dp)
                 .align(Alignment.BottomStart),
             text = "BottomStart")
 
         Text(style = MaterialTheme.typography.h6,
-            modifier = Modifier.background(Color.Yellow)
+            modifier = Modifier
+                .background(Color.Yellow)
                 .padding(6.dp)
                 .align(Alignment.BottomCenter),
             text = "BottomCenter")
 
         Text(style = MaterialTheme.typography.h6,
-            modifier = Modifier.background(Color.Yellow)
+            modifier = Modifier
+                .background(Color.Yellow)
                 .padding(6.dp)
                 .align(Alignment.BottomEnd),
             text = "BottomEnd")
 
+    }
+
+}
+
+
+@Composable
+fun BoxExample3(){
+    Box (){
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "images",
+            Modifier
+                .size(300.dp, 300.dp)
+                .align(Alignment.TopCenter))
+        Text(text = "Image",
+            style = MaterialTheme.typography.h6,
+        color = Color.Black,
+        modifier = Modifier.align(Alignment.BottomCenter))
+
+        Button(onClick = {},
+        colors = ButtonDefaults.textButtonColors(
+            backgroundColor = Color.White,
+            contentColor = Color.DarkGray
+        ),
+        modifier = Modifier
+            .align(Alignment.TopEnd)
+            .padding(10.dp)
+            .border(3.dp, color = Color.DarkGray, RectangleShape)) {
+Text(text = "Add To Cart")
+        }
     }
 }
 
